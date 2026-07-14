b0d75f3fc99a9d5ff5221787018f84a53750e9e5
Maksym Shostak <MaksymShostak@users.noreply.github.com>
2026-07-14 12:03:04 +0300

    Update ontology import catalog

--- src/app/js/owl2vowl.js
+++ src/app/js/owl2vowl.js
@@ -1518,13 +1518,27 @@ module.exports = function owl2vowl(xmlString) {
 };
 
 const ONTOLOGY_CATALOG = {
-  "http://purl.org/dc/elements/1.1/": "../external/dc.rdf",
-  "http://purl.org/dc/terms/": "../external/dcterms.rdf",
+  "http://purl.org/dc/elements/1.1": "../external/dc.rdf",
+  "http://purl.org/dc/terms": "../external/dcterms.rdf",
+  "http://purl.org/goodrelations/v1": "../external/goodrelations.owl",
+  "http://purl.org/linked-data/cube": "../external/cube.rdf",
+  "http://purl.org/ontology/bibo": "../external/bibo.rdf.xml",
+  "http://purl.org/vocab/vann": "../external/vann-vocab-20100607.rdf",
+  "http://rdfs.org/ns/void": "../external/void.ttl",
+  "http://rdfs.org/sioc/ns": "../external/sioc.rdf",
+  "http://schema.org": "../external/schemaorg.owl",
+  "http://usefulinc.com/ns/doap": "../external/doap.rdf",
+  "http://www.w3.org/2003/01/geo/wgs84_pos": "../external/wgs84_pos.rdf",
   "http://www.w3.org/2004/02/skos/core": "../external/skos.rdf",
+  "http://www.w3.org/2006/time": "../external/time.rdf",
   "http://www.w3.org/ns/dcat": "../external/dcat3.rdf",
-  "http://www.w3.org/2006/time": "https://raw.githubusercontent.com/w3c/sdw/gh-pages/time/rdf/time.rdf",
+  "http://www.w3.org/ns/org": "../external/org.rdf",
+  "http://www.w3.org/ns/prov": "../external/prov.owl",
+  "http://www.w3.org/ns/sosa": "../external/sosa.ttl",
+  "http://www.w3.org/ns/ssn": "../external/ssn.ttl",
   "http://www.w3.org/ns/time/gregorian": "../external/time-gregorian.rdf",
-  "http://xmlns.com/foaf/0.1/": "../external/foaf.rdf"
+  "http://xmlns.com/foaf/0.1": "../external/foaf.rdf",
+  "https://schema.org": "../external/schemaorg.owl"
 };
 
 /**
